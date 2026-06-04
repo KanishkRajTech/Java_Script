6516b75b09550607de038dcf0b5ff1143a112f58
KANISHK RAJ <kanishkraj601@gmail.com>
2026-06-04 22:33:43 +0530

    object part 1

--- Day7/object.js
+++ Day7/object.js
@@ -1,7 +1,28 @@
+
+const user = Symbol("user");
 const JsUser = {
     name: "Kanishk Raj",
     age: 21,
+    [user]: user,
     city: "Katihar, Bihar"
 };
 
 console.log(JsUser.name); 
+console.log(JsUser.age);
+console.log(JsUser[user]);
+console.log(JsUser["city"]);
+
+JsUser.name = "Kanishk";
+console.log(JsUser.name); // Kanishk
+
+// Object.freeze(JsUser);
+JsUser.name = "Raj";
+console.log(JsUser.name); // Kanishk
+
+JsUser.greeting = function() {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+}
+console.log(JsUser.greeting);  // [Function: (anonymous)]
+console.log(JsUser.greeting()); // Hello, my name is Kanishk and I am 21 years old
+
+
